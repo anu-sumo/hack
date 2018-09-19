c02db7d00ce448c9b3fec787b4a13a8dc2c0b9f5
Anurag Nanda <anurag@sumologic.com>
2018-09-18 18:47:45 -0700

    alram setup

--- chrome/extension/background.js
+++ chrome/extension/background.js
@@ -1,5 +1,7 @@
 const bluebird = require('bluebird');
 
+console.log('background');
+
 global.Promise = bluebird;
 
 function promisifier(method) {
@@ -16,6 +18,24 @@ function promisifier(method) {
 function promisifyAll(obj, list) {
   list.forEach(api => bluebird.promisifyAll(obj[api], { promisifier }));
 }
+
+console.log('background.js')
+chrome.alarms.create('checkStatus', {
+  delayInMinutes: 0.1, periodInMinutes: 0.1
+});
+
+
+// chrome.alarms.onAlarm.addListener(function (alarm) {
+//   console.log(alarm);
+
+//   chrome.notifications.create('reminder' + alarm.scheduledTime, {
+//     type: 'basic',
+//     iconUrl: "img/icon-48.png",
+//     title: 'Don\'t forget!',
+//     message: 'bla bla sl'
+//   }, function (notificationId) { });
+
+// });
 
 // let chrome extension api support Promise
 promisifyAll(chrome, [
