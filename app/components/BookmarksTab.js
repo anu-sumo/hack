4abeb7dfa4e5e0e000ab94e02f43d6b2fa46eeb5
Tarun Kumar <tarun@sumologic.com>
2018-09-22 15:37:14 -0700

    SUMO-0 more renaming and different logo for confluence

--- app/components/BookmarksTab.js
+++ app/components/BookmarksTab.js
@@ -15,6 +15,7 @@ import ExpandMore from '@material-ui/icons/ExpandMore';
 import StarBorder from '@material-ui/icons/StarBorder';
 import sumoLogo from '../../chrome/assets/img/sumo.png'
 import jiraLogo from '../../chrome/assets/img/jira.png';
+import confluenceLogo from '../../chrome/assets/img/confluence.png';
 import githubLogo from '../../chrome/assets/img/github.png';
 import jenkinsLogo from '../../chrome/assets/img/jenkins.png';
 
@@ -33,17 +34,17 @@ class NestedList extends React.Component {
   constructor(props) {
     super(props);
     this.bookmarks = {
-      "nite": { icon: sumoLogo, url: "https://nite-www.sumologic.net/ui/index.html?customerId=0000000131" },
-      "stag": { icon: sumoLogo, url: "https://stag-www.sumologic.net/ui/index.html?customerId=0000000475" },
-      "long-prod": { icon: sumoLogo, url: "https://long-prod.sumologic.net/ui/index.html?customerId=0000000005" },
-      "long-us2": { icon: sumoLogo, url: "https://long-us2.sumologic.net" },
-      "long-dub": { icon: sumoLogo, url: "https://long-dub.sumologic.net" },
-      "long-syd": { icon: sumoLogo, url: "https://long-syd.sumologic.net" },
-      prod: { icon: sumoLogo, url: "https://service.sumologic.com/ui" },
-      wiki: { icon: jiraLogo, url: "https://wiki.kumoroku.com/confluence/display/" },
-      jira: { icon: jiraLogo, url: "https://jira.kumoroku.com" },
-      jenkins: { icon: jenkinsLogo, url: "https://jenkins.kumoroku.com" },
-      codelabs: { icon: githubLogo, url: "https://github.com/Sanyaku/codelabs" },
+      "Sumo Nite UI": { icon: sumoLogo, url: "https://nite-www.sumologic.net/ui/index.html?customerId=0000000131" },
+      "Sumo Stag UI": { icon: sumoLogo, url: "https://stag-www.sumologic.net/ui/index.html?customerId=0000000475" },
+      "Sumo Long-Prod UI": { icon: sumoLogo, url: "https://long-prod.sumologic.net/ui/index.html?customerId=0000000005" },
+      "Sumo Long-Us2 UI": { icon: sumoLogo, url: "https://long-us2.sumologic.net" },
+      "Sumo Long-Dub UI": { icon: sumoLogo, url: "https://long-dub.sumologic.net" },
+      "Sumo Long-Syd UI": { icon: sumoLogo, url: "https://long-syd.sumologic.net" },
+      "Sumo Prod UI": { icon: sumoLogo, url: "https://service.sumologic.com/ui" },
+      "Wiki": { icon: confluenceLogo, url: "https://wiki.kumoroku.com/confluence/display/" },
+      "Jira": { icon: jiraLogo, url: "https://jira.kumoroku.com" },
+      "Jenkins": { icon: jenkinsLogo, url: "https://jenkins.kumoroku.com" },
+      "Codelabs": { icon: githubLogo, url: "https://github.com/Sanyaku/codelabs" },
     };
   }
 
